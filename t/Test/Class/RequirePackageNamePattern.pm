bd4c7635c1524f241d457cd619df38f6a86ecff2
jonasbn <jonasbn@logicLAB.dk>
2013-12-02 08:25:34 +0100

    Added Test::Class based test suite and fixes a bug in the main module added some todos for future improvements

---
+++ t/Test/Class/RequirePackageNamePattern.pm
@@ -0,0 +1,163 @@
+package Test::Class::RequirePackageNamePattern;
+
+# $Id$
+
+use strict;
+use warnings;
+
+use Env qw($TEST_VERBOSE);
+use Data::Dumper;
+use Test::More qw(no_plan);
+use base qw(Test::Class);
+use lib qw(lib);
+
+sub startup : Test(startup) {
+    my $self = shift;
+
+    require Perl::Critic;
+    use Perl::Critic::Policy::logicLAB::RequirePackageNamePattern;
+
+    $self->{perl_string} = join '', <DATA>;
+}
+
+sub no_requirement : Test(1) {
+    my $self = shift;
+
+    my $critic = Perl::Critic->new(
+        '-profile'       => '',
+        '-single-policy' => 'logicLAB::RequirePackageNamePattern'
+    );
+
+    my @p = $critic->policies;
+    is( scalar @p, 1, 'single policy RequirePackageNamePattern' );
+
+    my $policy = $p[0];
+
+    if ($TEST_VERBOSE) {
+        diag Dumper $policy;
+    }
+
+    my @violations = $critic->critique( \$self->{perl_string} );
+
+    is( scalar @violations, 0 );
+
+    foreach my $violation (@violations) {
+        is( $violation->explanation,
+            q{Use specified requirement for package naming for This::Is::A::Test} );
+        is( $violation->description,
+            q{Package name: This::Is::A::Test is not complying with required standard} );
+    }
+
+    if ($TEST_VERBOSE) {
+        diag Dumper \@violations;
+    }
+}
+
+sub requiring_presence : Test(4) {
+    my $self = shift;
+
+    my $critic = Perl::Critic->new(
+        '-profile'       => 't/presence.conf',
+        '-single-policy' => 'logicLAB::RequirePackageNamePattern'
+    );
+
+    my @p = $critic->policies;
+    is( scalar @p, 1, 'single policy RequirePackageNamePattern' );
+
+    my $policy = $p[0];
+
+    if ($TEST_VERBOSE) {
+        diag Dumper $policy;
+    }
+
+    my @violations = $critic->critique( \$self->{perl_string} );
+
+    is( scalar @violations, 1 );
+
+    foreach my $violation (@violations) {
+        is( $violation->explanation,
+            q{Use specified requirement for package naming for This::Is::A::Test} );
+        is( $violation->description,
+            q{Package name: This::Is::A::Test is not complying with required standard} );
+    }
+
+    if ($TEST_VERBOSE) {
+        diag Dumper \@violations;
+    }
+}
+
+sub requiring_prefix : Test(4) {
+	my $self = shift;
+
+    my $critic = Perl::Critic->new(
+        '-profile'       => 't/prefix.conf',
+        '-single-policy' => 'logicLAB::RequirePackageNamePattern'
+    );
+
+    my @p = $critic->policies;
+    is( scalar @p, 1, 'single policy RequirePackageNamePattern' );
+
+    my $policy = $p[0];
+
+    if ($TEST_VERBOSE) {
+        diag Dumper $policy;
+    }
+
+    my @violations = $critic->critique( \$self->{perl_string} );
+
+    is( scalar @violations, 1 );
+
+    foreach my $violation (@violations) {
+        is( $violation->explanation,
+            q{Use specified requirement for package naming for This::Is::A::Test} );
+        is( $violation->description,
+            q{Package name: This::Is::A::Test is not complying with required standard} );
+    }
+
+    if ($TEST_VERBOSE) {
+        diag Dumper \@violations;
+ 	}
+ }
+
+sub requiring_postfix : Test(4) {
+    my $self = shift;
+
+    my $critic = Perl::Critic->new(
+        '-profile'       => 't/postfix.conf',
+        '-single-policy' => 'logicLAB::RequirePackageNamePattern'
+    );
+
+    my @p = $critic->policies;
+    is( scalar @p, 1, 'single policy RequirePackageNamePattern' );
+
+    my $policy = $p[0];
+
+    if ($TEST_VERBOSE) {
+        diag Dumper $policy;
+    }
+
+    my @violations = $critic->critique( \$self->{perl_string} );
+
+    is( scalar @violations, 1 );
+
+    foreach my $violation (@violations) {
+        is( $violation->explanation,
+            q{Use specified requirement for package naming for This::Is::A::Test} );
+        is( $violation->description,
+            q{Package name: This::Is::A::Test is not complying with required standard} );
+    }
+
+    if ($TEST_VERBOSE) {
+        diag Dumper \@violations;
+    }
+}
+
+1;
+
+__DATA__
+package This::Is::A::Test;
+
+use strict;
+use warnings;
+
+1;
